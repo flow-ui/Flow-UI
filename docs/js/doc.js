967a7473584ab281466c6de819b05919dc84c5b4
tower1229 <tower1229@qq.com>
2016-12-05 12:44:01 +0800

    报错

--- docs/js/doc.js
+++ docs/js/doc.js
@@ -109,7 +109,7 @@ define(function(require) {
 		var cons = console;
 		if (cons) {
 			cons.log("%c\n	", "font-size:130px;background:url('http://g.hiphotos.bdimg.com/album/s%3D1100%3Bq%3D90/sign=51698e337d3e6709ba0041fe0bf7a44c/08f790529822720efa1c90ee79cb0a46f31fabd0.jpg') no-repeat");
-			cons.log("hello," + base.getType());
+			cons.log("hello, u" );
 		}
 	}
 
