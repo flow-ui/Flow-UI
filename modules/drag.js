6022f734fdb29f0c622938492455e77e186de84b
tower1229 <tower1229@qq.com>
2016-12-09 17:08:43 +0800

    drag

--- modules/drag.js
+++ modules/drag.js
@@ -1,8 +1,8 @@
 /*
  * name: drag
- * vertion: v0.7.0
- * update: 支持批量拖拽
- * date: 2016-12-08
+ * vertion: v0.7.1
+ * update: 优化
+ * date: 2016-12-09
  */
 define('drag', function(require, exports, module) {
     'use strict';
@@ -16,31 +16,35 @@ define('drag', function(require, exports, module) {
         drag: null,
         dragEnd: null
     };
-    var moveIt = function(ele, dir, offset) {
-        if (base.browser.support3d) {
-            var requestAnimationFrame = window.requestAnimationFrame || function(cb) {
-                setTimeout(cb, 0);
-            };
-            var translateX = $(ele).data('translateX') || '0px';
-            var translateY = $(ele).data('translateY') || '0px';
-            var startX = $(ele).data('start').x;
-            var startY = $(ele).data('start').y;
-            var movecb = $(ele).data('movecb');
-            if (dir === 'left') {
-                translateX = (offset - parseInt(startX)) + 'px';
-            } else if (dir === 'top') {
-                translateY = (offset - parseInt(startY)) + 'px';
+    var moveTimer;
+    var moveIt = function(ele, offset) {
+        if (!moveTimer) {
+            if (base.browser.support3d) {
+                var translateX = $(ele).data('translateX') || '0px';
+                var translateY = $(ele).data('translateY') || '0px';
+                var startX = $(ele).data('start').x;
+                var startY = $(ele).data('start').y;
+                var movecb = $(ele).data('movecb');
+                translateX = (offset[0] - parseInt(startX)) + 'px';
+                translateY = (offset[1] - parseInt(startY)) + 'px';
+                moveTimer = setTimeout(function() {
+                    $(ele)
+                        .data('translateX', translateX)
+                        .data('translateY', translateY)
+                        .get(0).style.transform = 'translate(' + translateX + ',' + translateY + ')';
+                    moveTimer = clearTimeout(moveTimer);
+                    typeof(movecb) === 'function' && movecb(ele);
+                }, 0);
             } else {
-                console.log('内部错误！');
+                moveTimer = setTimeout(function() {
+                    $(ele).css({
+                        left: offset[0],
+                        top: offset[1]
+                    });
+                    moveTimer = clearTimeout(moveTimer);
+                    typeof(movecb) === 'function' && movecb(ele);
+                }, 1000 / 60);
             }
-            $(ele).data('translateX', translateX);
-            $(ele).data('translateY', translateY);
-            requestAnimationFrame(function() {
-                $(ele).get(0).style.transform = 'translate(' + translateX + ',' + translateY + ')';
-                typeof(movecb) === 'function' && movecb(ele);
-            });
-        } else {
-            $(ele).css(dir, offset);
         }
     };
     $.fn.drag = function(config) {
@@ -54,14 +58,13 @@ define('drag', function(require, exports, module) {
                 cssobj = {
                     "cursor": "move"
                 };
-            if($this.data('draginit')){
+            if ($this.data('draginit')) {
                 return null;
             }
-            $this.data('draginit',1);
             if (thisPosition !== 'absolute' && thisPosition !== 'fixed') {
                 cssobj.position = 'relative';
             }
-            $this.css(cssobj).data('movecb', opt.drag);
+            $this.css(cssobj).data('movecb', opt.drag).data('draginit', 1);
             thisPosition = cssobj = null;
             if (opt.wrap === null) {
                 if ($this.parent().is('body')) {
@@ -81,40 +84,41 @@ define('drag', function(require, exports, module) {
                 var mousemove = function(e) {
                         e.preventDefault();
                         var rx = parseInt(ox - mx + e.clientX),
-                            ry = parseInt(oy - my + e.clientY);
+                            ry = parseInt(oy - my + e.clientY),
+                            movex, movey;
                         // drective X
                         if ((rx < 0 && fw > ow) || (rx > 0 && fw < ow)) {
                             if (!opt.overflow) {
-                                moveIt($this, 'left', 0);
+                                movex = 0;
                             } else {
-                                moveIt($this, 'left', rx);
+                                movex = rx;
                             }
                         } else if ((rx > 0 && rx > (fw - ow)) || (rx < 0 && rx < (fw - ow))) {
                             if (!opt.overflow) {
-                                moveIt($this, 'left', fw - ow);
+                                movex = fw - ow;
                             } else {
-                                moveIt($this, 'left', rx);
+                                movex = rx;
                             }
                         } else {
-                            moveIt($this, 'left', rx);
+                            movex = rx;
                         }
                         // drective Y
                         if ((ry < 0 && fw > ow) || (ry > 0 && fw < ow)) {
                             if (!opt.overflow) {
-                                moveIt($this, 'top', 0);
+                                movey = 0;
                             } else {
-                                moveIt($this, 'top', ry);
+                                movey = ry;
                             }
                         } else if ((ry > 0 && ry > (fh - oh)) || (ry < 0 && ry < (fh - oh))) {
                             if (!opt.overflow) {
-                                moveIt($this, 'top', fh - oh);
+                                movey = fh - oh;
                             } else {
-                                moveIt($this, 'top', ry);
+                                movey = ry;
                             }
                         } else {
-                            moveIt($this, 'top', ry);
+                            movey = ry;
                         }
-                        return false;
+                        return moveIt($this, [rx, ry]);
                     },
                     mouseup = function() {
                         $(document).unbind('mousemove', mousemove).unbind('mouseup', mouseup);
@@ -124,9 +128,9 @@ define('drag', function(require, exports, module) {
                     ox = parseInt($this.offset().left) || 0;
                     oy = parseInt($this.offset().top) || 0;
                     if (!$this.data('start')) {
-                        $this.data('start',{
-                            x:ox,
-                            y:oy
+                        $this.data('start', {
+                            x: ox,
+                            y: oy
                         });
                     }
                     mx = e.clientX;
