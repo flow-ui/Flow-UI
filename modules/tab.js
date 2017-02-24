17640eea342eb52d8edf2e359fc19c27eb9b7d40
tower1229 <tower1229@qq.com>
2017-02-24 15:16:09 +0800

    tab.js 2.2.1

--- modules/tab.js
+++ modules/tab.js
@@ -1,32 +1,33 @@
 /*
  * name: tab.js
- * version: v2.2.0
- * update: 增加tab_t.last
- * date: 2016-06-20
+ * version: v2.2.1
+ * update: js-lint
+ * date: 2017-02-24
  */
 define('tab', function(require, exports, module) {
-	var $ = require('jquery');
-	seajs.importStyle('.tab .tab_t{cursor:pointer}.tab .tab_c{display:none}.tab .tab_c_cur{display:block}', module.uri)
+	'use strict';
+	seajs.importStyle('.tab .tab_t{cursor:pointer}.tab .tab_c{display:none}.tab .tab_c_cur{display:block}', module.uri);
+	var $ = require('jquery'),
+		def = {
+			tabs: ".tab_t", //标签
+			conts: ".tab_c", //内容
+			width_auto: false, //标签自适应宽度
+			posi_auto: true, //标签自动定位，关闭后可以样式控制
+			left: 0, //第一个标签距左起始位置
+			margin: 0, //标签间距
+			auto: false, //是否自动播放，默认否
+			interval: 5e3, //自动播放间隔，默认5s
+			start: 0, //初始显示，默认第一个
+			timeout: 0, //触发延迟
+			act: 'click', //触发动作
+			beforeChange:function(){},//切换前，return false将终止切换
+			callback: function() {}, //回调方法 @param ($this,$tab_t,index) : 当前对象，标签，当前帧序号
+			ext: function() {} //扩展方法 @param ($this,$tab_t,opts) : 当前对象，标签，配置
+		};
 
-	var def = {
-		tabs: ".tab_t", //标签
-		conts: ".tab_c", //内容
-		width_auto: false, //标签自适应宽度
-		posi_auto: true, //标签自动定位，关闭后可以样式控制
-		left: 0, //第一个标签距左起始位置
-		margin: 0, //标签间距
-		auto: false, //是否自动播放，默认否
-		interval: 5e3, //自动播放间隔，默认5s
-		start: 0, //初始显示，默认第一个
-		timeout: 0, //触发延迟
-		act: 'click', //触发动作
-		beforeChange:function(){},//切换前，return false将终止切换
-		callback: function() {}, //回调方法 @param ($this,$tab_t,index) : 当前对象，标签，当前帧序号
-		ext: function() {} //扩展方法 @param ($this,$tab_t,opts) : 当前对象，标签，配置
-	}
 	$.fn.tab = function(config) {
 		return $(this).each(function(i, e) {
-			var $this = $(e).addClass('tab tabID' + parseInt(Math.random() * 1e5)),
+			var $this = $(e),
 				opt = $.extend({}, def, opt, config || {}),
 				thisPosition = $this.css('position'),
 				toggletab, tab_t_collect, tiemout, $tab_t, $tab_t_avail, $tab_c;
@@ -34,23 +35,23 @@ define('tab', function(require, exports, module) {
 			if (thisPosition !== 'absolute' && thisPosition !== 'fixed') {
 				thisPosition = 'relative';
 			}
-			$this.css('position', thisPosition).fadeIn(160);
+			$this.addClass('tab tabID' + parseInt(Math.random() * 1e5)).css('position', thisPosition).show();
 
 			$tab_t = $this.find(opt.tabs);
 			if (!$tab_t.length) {
 				console.log('tabs not exists');
-				return $(this);
+				return $this;
 			}
 			$tab_t_avail = $tab_t.filter(function() {
 				if ($(this).attr('ignore') === void(0)) {
-					return $(this);
+					return $this;
 				}
 			}).addClass('tab_available');
 
 			$tab_c = $this.find(opt.conts).addClass('tab_c');
 			if (!$tab_c.length) {
 				console.log('tabContent not exists');
-				return $(this);
+				return $this;
 			}
 			toggletab = function(i) {
 				$tab_t_avail.eq(i).addClass('tab_t_cur').siblings().removeClass('tab_t_cur');
@@ -65,21 +66,22 @@ define('tab', function(require, exports, module) {
 			}
 			if (opt.width_auto) {
 				$tab_t.width(1 / $tab_t.length * 100 + '%');
-			};
+			}
 
 			$tab_t.each(function(i, e) {
 				if (opt.posi_auto) {
 					var leftPx = 0;
-					for (var _i = 0; _i < i; _i++) {
+					var _i = 0;
+					for (; _i < i; _i++) {
 						leftPx += parseFloat($tab_t.eq(_i).outerWidth(true));
-					};
+					}
 					$(e).css({
 						position: 'absolute',
 						top: 0,
 						'left': opt.width_auto ? 1 / $tab_t.length * 100 * i + '%' : leftPx + opt.left + opt.margin * i
 					});
 				}
-				if (!i) {
+				if (i===0) {
 					$(e).addClass('first');
 				}
 				if($tab_t.length-i===1){
@@ -95,50 +97,48 @@ define('tab', function(require, exports, module) {
 					if (event.timeStamp) {
 						_last = event.timeStamp;
 						_timeout = setTimeout(function() {
-							if (_last - event.timeStamp == 0) {
+							if (_last - event.timeStamp === 0) {
 								toggletab(index);
-							};
+							}
 						}, opt.tiemout);
 					} else {
 						toggletab(index);
-					};
-				};
+					}
+				}
 				setTimeout(function() {
 					typeof(opt.callback)==='function' && opt.callback($this, $tab_t, index);
 					index = _timeout = _last = null;
 				}, 0);
 			});
-
 			//启动
 			$tab_t_avail.eq(opt.start).trigger(opt.act);
 			$this.data('tabruning');
 			//扩展
 			opt.ext && opt.ext($this, $tab_t, opt);
-
 			//自动播放
 			if ($tab_t_avail.length > 1 && opt.auto) {
-				var i = opt.start,
+				var _start = opt.start,
 					auto = function() {
-						i = i >= $tab_t_avail.length - 1 ? 0 : ++i;
-						$tab_t_avail.eq(i).trigger(opt.act);
+						_start = _start >= $tab_t_avail.length - 1 ? 0 : ++_start;
+						$tab_t_avail.eq(_start).trigger(opt.act);
 					},
 					t = setInterval(function() {
-						auto()
+						auto();
 					}, opt.interval);
 				$tab_c.hover(function() {
 					clearInterval(t);
 				}, function() {
 					t = setInterval(function() {
-						auto()
+						auto();
 					}, opt.interval);
 				});
 				$this.parent().on('DOMNodeRemoved',function(e){
-					if($(e.target).is($this)){
+					if($(e.target).is($this) && t){
 						//DOM移除后释放全局变量
-						t && clearInterval(t);
+						clearInterval(t);
 					}
 				});
-			};
-		})
+			}
+		});
 	};
 });
