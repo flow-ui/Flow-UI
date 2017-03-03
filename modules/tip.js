647e19a54f6c95c180465469ec619a6b08a60036
tower1229 <tower1229@qq.com>
2017-03-03 12:21:50 +0800

    tip.js v1.3.1

--- modules/tip.js
+++ modules/tip.js
@@ -1,15 +1,32 @@
 /*
  * name: tip.js
- * version: v1.2.5
- * update: 遮罩层样式内嵌防止干扰
- * date: 2016-11-29
+ * version: v1.3.1
+ * update: 修复hover触发bug
+ * date: 2017-03-03
  */
 define('tip', function(require, exports, module) {
 	'use strict';
-	seajs.importStyle('.tip-box{display:none;position:absolute;z-index:99}.tip-title{background:#eee;padding:5px 10px;border:1px solid #ccc;border-bottom:0;border-radius:4px 4px 0 0}.tip-content{background:#fff;color:#000;line-height:1.5em;padding:8px 20px;border:1px solid #ccc}.tip-withContent .tip-object{display:none}.tip-withObject .tip-content{display:none}.tip-noTitle .tip-content{border-radius:4px;box-shadow:rgba(0,0,0,.3)}.tip-noTitle .tip-title{display:none}.tip-withTitle .tip-content{border-radius:0 0 4px 4px}.tip-arr,.tip-arr-cell{position:absolute;width:0;height:0;overflow:hidden;border:6px solid transparent}.tip-left .tip-arr{border-left-color:#ccc;right:-12px;top:50%;margin-top:-6px}.tip-left .tip-arr-cell{border-left-color:#fff;right:-11px;top:50%;margin-top:-6px}.tip-right .tip-arr{border-right-color:#ccc;left:-12px;top:50%;margin-top:-6px}.tip-right .tip-arr-cell{border-right-color:#fff;left:-11px;top:50%;margin-top:-6px}.tip-top .tip-arr{border-top-color:#ccc;bottom:-12px;left:50%;margin-left:-6px}.tip-top .tip-arr-cell{border-top-color:#fff;bottom:-11px;left:50%;margin-left:-6px}.tip-bottom .tip-arr{border-bottom-color:#ccc;top:-12px;left:50%;margin-left:-6px}.tip-bottom .tip-arr-cell{border-bottom-color:#fff;top:-11px;left:50%;margin-left:-6px}', module.uri);
+	seajs.importStyle('.tip-box{display:none;position:absolute;z-index:99}\
+		.tip-title{background:#eee;padding:5px 10px;border:1px solid #ccc;border-bottom:0;border-radius:4px 4px 0 0}\
+		.tip-content{background:#fff;color:#000;line-height:1.5em;padding:8px 20px;border:1px solid #ccc}\
+		.tip-withContent .tip-object{display:none}\
+		.tip-withObject .tip-content{display:none}\
+		.tip-noTitle .tip-content{border-radius:4px;box-shadow:rgba(0,0,0,.3)}\
+		.tip-noTitle .tip-title{display:none}\
+		.tip-withTitle .tip-content{border-radius:0 0 4px 4px}\
+		.tip-arr,.tip-arr-cell{position:absolute;width:0;height:0;overflow:hidden;border:6px solid transparent}\
+		.tip-left .tip-arr{border-left-color:#ccc;right:-12px;top:50%;margin-top:-6px}\
+		.tip-left .tip-arr-cell{border-left-color:#fff;right:-11px;top:50%;margin-top:-6px}\
+		.tip-right .tip-arr{border-right-color:#ccc;left:-12px;top:50%;margin-top:-6px}\
+		.tip-right .tip-arr-cell{border-right-color:#fff;left:-11px;top:50%;margin-top:-6px}\
+		.tip-top .tip-arr{border-top-color:#ccc;bottom:-12px;left:50%;margin-left:-6px}\
+		.tip-top .tip-arr-cell{border-top-color:#fff;bottom:-11px;left:50%;margin-left:-6px}\
+		.tip-bottom .tip-arr{border-bottom-color:#ccc;top:-12px;left:50%;margin-left:-6px}\
+		.tip-bottom .tip-arr-cell{border-bottom-color:#fff;top:-11px;left:50%;margin-left:-6px}', module.uri);
 	var $ = require('jquery'),
 		def = {
-			act: 'mouseenter', // mouseenter | click
+			el: null,
+			trigger: 'hover', // hover | click | custom
 			place: 'left-center', // [posi]-[posi]-[in or null]，前两项必须，表示位置，第三项表示从内部定位，可省
 			title: false, // title text | false
 			hasarr: true, // 有无箭头
@@ -18,207 +35,227 @@ define('tip', function(require, exports, module) {
 			modal: false, // 模态，会增加一个半透明背景
 			opacity: 0.5, // 背景透明度
 			hook: null, // 自定义class钩子
-			show: false, // 立即显示，不会添加opt.act事件绑定
+			show: false, // 立即显示
 			onshow: function($this) {},
 			onclose: function() {}
 		},
-		wrap = $('<div class="tip-box" id="tip-box">\
-<div class="tip-title" id="tip-title"></div>\
-<div class="tip-content" id="tip-content"></div>\
-<div class="tip-object" id="tip-object"></div>\
-<i class="tip-arr" id="tip-arr"></i><i class="tip-arr-cell" id="tip-arr-cell"></i>\
-</div>');
+		tipBoxHtml = '<div class="tip-box" id="tip-box">\
+	<div class="tip-title" id="tip-title"></div>\
+	<div class="tip-content" id="tip-content"></div>\
+	<div class="tip-object" id="tip-object"></div>\
+	<i class="tip-arr" id="tip-arr"></i><i class="tip-arr-cell" id="tip-arr-cell"></i>\
+</div>',
+		$blank = $("#boxBlank"),
+		$tipbox = $('#tip-box'),
+		closeTip = function($this, opt) {
+			$tipbox.stop(1).fadeOut(160).find('#tip-object').empty();
+			$this.removeClass('showTip');
+			$blank.hide();
+			if (typeof(opt.onclose) === 'function') opt.onclose();
+		};
+	if ($blank.length) {
+		$blank = $('<div id="boxBlank" style="position:fixed;z-index:98;left:0;top:0;width:100%;height:100%;background: #000;" onselectstart="return false" />').css('opacity', opt.opacity);
+		$('body').append($blank);
+	}
 
-	$.fn.tip = function(tip, config) {
-		var opt = $.extend({}, def, config || {}),
-			$blank;
-		if ($("#boxBlank").length) {
-			$('body').append('<div id="boxBlank" style="position:fixed;z-index:98;left:0;top:0;width:100%;height:0;background: #000;filter: alpha(opacity=50);background: rgba(0,0,0,.5);" onselectstart="return false" />');
-		}
-		$blank = $('#boxBlank').css('opacity', opt.opacity);
-		if (!$('#tip-box').length) {
-			$('body').append(wrap);
-		}
-		$(this).each(function(i, e) {
+	if (!$tipbox.length) {
+		$tipbox = $(tipBoxHtml);
+		$('body').append($tipbox);
+	}
+
+	var Tip = function(tip, config) {
+		var opt = $.extend({}, def, config || {});
+		var $el = $(opt.el);
+
+		$el.each(function(i, e) {
 			var $this = $(e),
 				place = opt.place.split('-'),
-				show,
-				hide = function() {
-					$('#tip-box').find('#tip-object').empty().end().stop(1).fadeOut(160);
-					$this.removeClass('showTip');
-					$blank.hide();
-					if (typeof(opt.onclose) === 'function') opt.onclose();
-				},
-				offArr = (opt.hasarr ? parseInt($('#tip-arr').css('border-top-width')) : 0) + opt.offset;
-			show = function() {
-				var _tipLeft,
-					_tipTop,
-					_offX,
-					_offY,
-					_getLeft,
-					_getTop,
-					_classCatch = '',
-					_tipConent = '',
-					_tipObj = '',
-					_title = '';
-				opt.hook && (_classCatch += ($.trim(opt.hook) + ' '));
+				offArr = (opt.hasarr ? parseInt($tipbox.find('#tip-arr').css('border-top-width')) : 0) + opt.offset,
+				show = function() {
+					var _tipLeft,
+						_tipTop,
+						_offX,
+						_offY,
+						_getLeft,
+						_getTop,
+						_classCatch = opt.hook ? ($.trim(opt.hook) + ' ') : '',
+						_tipConent = '',
+						_tipObj = '',
+						_title = '';
 
-				if (typeof(tip) === 'object' && !!tip.length || ($.parseHTML($.trim(tip + ''))[0].nodeType === 1)) {
-					//现有dom或dom字符串
-					if (opt.type == 'content') {
+					if (typeof(tip) === 'object' && !!tip.length || ($.parseHTML($.trim(tip + ''))[0].nodeType === 1)) {
+						//现有dom或dom字符串
+						if (opt.type == 'content') {
+							_classCatch += 'tip-withContent ';
+						} else {
+							_classCatch += 'tip-withObject ';
+						}
+						if ($(tip).get(0).tagName === 'IMG') {
+							//ie8图片无法撑开宽度bug
+							_tipObj = $('<div />').width($(tip).width()).append($(tip).show());
+						} else {
+							_tipObj = $(tip).show();
+						}
+					} else {
+						//字符串或数字
 						_classCatch += 'tip-withContent ';
+						_tipConent = tip;
+					}
+
+					if (opt.title) {
+						_classCatch += 'tip-withTitle ';
+						_title = opt.title;
 					} else {
-						_classCatch += 'tip-withObject ';
+						_classCatch += 'tip-noTitle ';
 					}
-					if ($(tip).get(0).tagName === 'IMG') {
-						//ie8图片无法撑开宽度bug
-						_tipObj = $('<div />').width($(tip).width()).append($(tip).show());
-					} else {
-						_tipObj = $(tip).show();
+
+					if (opt.hasarr) {
+						_classCatch += 'tip-' + place[0];
 					}
-				} else {
-					//字符串或数字
-					_classCatch += 'tip-withContent ';
-					_tipConent = tip;
-				}
+					$tipbox
+						.removeClass('tip-withContent tip-withObject tip-withTitle tip-noTitle tip-left tip-top tip-right tip-bottom')
+						.addClass(_classCatch)
+						.find('#tip-object').html(_tipObj).end()
+						.find('#tip-content').text(_tipConent).end()
+						.find('#tip-title').text(_title);
 
-				if (opt.title) {
-					_classCatch += 'tip-withTitle ';
-					_title = opt.title;
-				} else {
-					_classCatch += 'tip-noTitle ';
-				}
-
-				if (opt.hasarr) {
-					_classCatch += 'tip-' + place[0];
-				}
-				$('#tip-box')
-					.removeClass('tip-withContent tip-withObject tip-withTitle tip-noTitle tip-left tip-top tip-right tip-bottom')
-					.addClass(_classCatch)
-					.find('#tip-object').html(_tipObj).end()
-					.find('#tip-content').text(_tipConent).end()
-					.find('#tip-title').text(_title);
-
-				if (_tipObj !== '' && opt.type === 'content') {
-					$('#tip-box').find('#tip-object').empty().end()
-						.find('#tip-content').html(_tipObj);
-				}
-				_offX = $('#tip-box').outerWidth() > $this.outerWidth() ? -Math.abs($this.outerWidth() - $('#tip-box').outerWidth()) / 2 : Math.abs($this.outerWidth() - $('#tip-box').outerWidth()) / 2;
-				_offY = $('#tip-box').outerHeight() > $this.outerHeight() ? -Math.abs($this.outerHeight() - $('#tip-box').outerHeight()) / 2 : Math.abs($this.outerHeight() - $('#tip-box').outerHeight()) / 2;
-				_getLeft = function() {
-					switch (place[1]) {
-						case 'center':
-							_tipLeft = $this.offset().left + _offX;
-							break;
-						case 'left':
-							_tipLeft = $this.offset().left;
-							break;
-						case 'right':
-							_tipLeft = $this.offset().left + $this.outerWidth() - $('#tip-box').outerWidth();
-							break;
-						default:
-							place[2] = place[1];
+					if (_tipObj !== '' && opt.type === 'content') {
+						$tipbox.find('#tip-object').empty().end()
+							.find('#tip-content').html(_tipObj);
 					}
-				};
-				_getTop = function() {
-					switch (place[1]) {
-						case 'center':
-							_tipTop = $this.offset().top + _offY;
-							break;
+					_offX = $tipbox.outerWidth() > $this.outerWidth() ? -Math.abs($this.outerWidth() - $tipbox.outerWidth()) / 2 : Math.abs($this.outerWidth() - $tipbox.outerWidth()) / 2;
+					_offY = $tipbox.outerHeight() > $this.outerHeight() ? -Math.abs($this.outerHeight() - $tipbox.outerHeight()) / 2 : Math.abs($this.outerHeight() - $tipbox.outerHeight()) / 2;
+					_getLeft = function() {
+						switch (place[1]) {
+							case 'center':
+								_tipLeft = $this.offset().left + _offX;
+								break;
+							case 'left':
+								_tipLeft = $this.offset().left;
+								break;
+							case 'right':
+								_tipLeft = $this.offset().left + $this.outerWidth() - $tipbox.outerWidth();
+								break;
+							default:
+								place[2] = place[1];
+						}
+					};
+					_getTop = function() {
+						switch (place[1]) {
+							case 'center':
+								_tipTop = $this.offset().top + _offY;
+								break;
+							case 'top':
+								_tipTop = $this.offset().top;
+								break;
+							case 'bottom':
+								_tipTop = $this.offset().top + $this.outerHeight() - $tipbox.outerHeight();
+								break;
+							default:
+								place[2] = place[1];
+						}
+					};
+					switch (place[0]) {
 						case 'top':
-							_tipTop = $this.offset().top;
+							_getLeft();
+							_tipTop = $this.offset().top - $tipbox.outerHeight() - offArr;
+							if (place[2] === 'in') {
+								_tipTop = _tipTop + $this.outerHeight();
+							}
 							break;
 						case 'bottom':
-							_tipTop = $this.offset().top + $this.outerHeight() - $('#tip-box').outerHeight();
+							_getLeft();
+							_tipTop = $this.offset().top + offArr;
+							if (place[2] !== 'in') {
+								_tipTop = _tipTop + $this.outerHeight();
+							}
+							break;
+						case 'right':
+							_getTop();
+							_tipLeft = $this.offset().left + offArr;
+							if (place[2] !== 'in') {
+								_tipLeft = _tipLeft + $this.outerWidth();
+							}
+							break;
+						case 'left':
+							_getTop();
+							_tipLeft = $this.offset().left - $tipbox.outerWidth() - offArr;
+
+							if (place[2] === 'in') {
+								_tipLeft = _tipLeft + $this.outerWidth();
+							}
 							break;
 						default:
-							place[2] = place[1];
+							//place[0]
 					}
+					$tipbox
+						.css({
+							'left': _tipLeft,
+							'top': _tipTop
+						}).stop(1).fadeIn(160);
+
+					$this.addClass('showTip');
+					if (opt.modal && opt.trigger === 'click') {
+						$blank.show();
+					}
+					typeof(opt.onshow) === 'function' && opt.onshow($this);
 				};
-				switch (place[0]) {
-					case 'top':
-						_getLeft();
-						_tipTop = $this.offset().top - $('#tip-box').outerHeight() - offArr;
-						if (place[2] === 'in') {
-							_tipTop = _tipTop + $this.outerHeight();
-						}
-						break;
-					case 'bottom':
-						_getLeft();
-						_tipTop = $this.offset().top + offArr;
-						if (place[2] !== 'in') {
-							_tipTop = _tipTop + $this.outerHeight();
-						}
-						break;
-					case 'right':
-						_getTop();
-						_tipLeft = $this.offset().left + offArr;
-						if (place[2] !== 'in') {
-							_tipLeft = _tipLeft + $this.outerWidth();
-						}
-						break;
-					case 'left':
-						_getTop();
-						_tipLeft = $this.offset().left - $('#tip-box').outerWidth() - offArr;
-
-						if (place[2] === 'in') {
-							_tipLeft = _tipLeft + $this.outerWidth();
-						}
-						break;
-				}
-				$('#tip-box')
-					.css({
-						'left': _tipLeft,
-						'top': _tipTop
-					}).stop(1).fadeIn(160);
-
-				$this.addClass('showTip');
-				opt.modal && opt.act == 'click' && $blank.height($(window).height()).show();
-				typeof(opt.onshow) === 'function' && opt.onshow($this);
-			};
 			if (opt.show) {
 				show();
-			} else {
-				if (opt.act === 'mouseenter') {
-					$this.on('mouseenter', function() {
-						show();
-						if (place[1] === 'in') {
-							$('#tip-box').on('mouseleave', function() {
-								hide();
-							});
-						}
-					});
-					if (place[1] !== 'in') {
-						$this.on('mouseleave', function() {
-							hide();
+			}
+			if (!$this.data('tipinit')) {
+				if (opt.trigger === 'hover') {
+					$this
+						.on('mouseenter', function() {
+							show();
+							$tipbox
+								.unbind()
+								.on('mouseenter', function() {
+									if ($this.timer) {
+										clearTimeout($this.timer);
+									}
+								})
+								.on('mouseleave', function() {
+									closeTip($this, opt);
+								});
+						})
+						.on('mouseleave', function() {
+							$this.timer = setTimeout(function() {
+								closeTip($this, opt);
+							}, 16);
 						});
-					}
-				} else if (opt.act === 'click') {
-					$('#tip-box').unbind();
+
+				} else if (opt.trigger === 'click') {
+					$tipbox.unbind();
 					$('body').on('click', function(e) {
 						if ($this.is(e.target) || $this.has(e.target).length) {
 							e.preventDefault();
 							if (!$('#tip-box:visible').length) {
-								setTimeout(function() {
-									show();
-								}, 0);
+								setTimeout(show, 0);
 							} else {
-								hide();
+								closeTip($this, opt);
 							}
-						} else if (!$('#tip-box').has(e.target).length) {
-							hide();
+						} else if (!$tipbox.has(e.target).length) {
+							closeTip($this, opt);
 						}
 					});
+				} else {
+					$this.on(opt.trigger, show);
 				}
+				$this.data('tipinit', true);
 			}
 		});
 		return {
 			hide: function() {
-				$('#tip-box').stop(1).fadeOut(160);
-				$(this).removeClass('showTip');
-				$("#boxBlank").hide();
-				if (typeof(opt.onclose) === 'function') opt.onclose();
+				closeTip($el, opt);
 			}
 		};
 	};
+	$.fn.tip = function(msg, config) {
+		return Tip(msg, $.extend(config, {
+			el: $(this)
+		}));
+	};
+
+	module.exports = Tip;
 });
