2602241f8434b4278239d6d17cc5214e0df149f1
tower1229 <tower1229@qq.com>
2017-03-09 16:57:14 +0800

    update docs && components

--- flow-ui/lib/sea.js
+++ flow-ui/lib/sea.js
@@ -61,6 +61,7 @@ seajs.config({
         'countdown'                         : 'v1.1.1',
         'datepicker'                        : 'v1.0.2',
         'drag'                              : 'v0.7.2',
+        'drag-panel'                        : 'v0.0.1',
         'dropdown'                          : 'v0.1.0',
         'easing'                            : 'v0.0.1',
         'echarts'                           : 'v0.0.2',
