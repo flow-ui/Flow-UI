479d1113f0680742e8e173a16f0e9dec7ddf7198
tower1229 <tower1229@qq.com>
2017-06-25 16:11:44 +0800

    update validform.js

--- modules/validform/validform.js
+++ modules/validform/validform.js
@@ -6,11 +6,12 @@
 */
 define('validform',function(require, exports, module) {
 	"use strict";
-	require('box');
+	
 	seajs.importStyle('.Validform_right{color:#71b83d}.Validform_wrong{color:red;white-space:nowrap}.Validform_loading{padding-left:20px}.Validform_error{background-color:#ffe7e7}.passwordStrength{display:block;height:18px;line-height:16px;clear:both;overflow:hidden;margin-bottom:5px}.passwordStrength b{font-weight:normal}.passwordStrength b,.passwordStrength span{display:inline-block;vertical-align:middle;line-height:16px;line-height:18px\9;height:16px}.passwordStrength span{width:63px;text-align:center;background-color:#d0d0d0;border-right:1px solid #fff}.passwordStrength .last{border-right:0;width:61px}.passwordStrength .bgStrength{color:#fff;background-color:#71b83d}'
 		,module.uri);
 	var $ = window.$ || require('jquery');
 	var ajaxRequest = $.ajax;
+	var box = require('box');
 	var win = window;
 	var undef = void 0;
 	var errorobj = null,
@@ -417,8 +418,8 @@ define('validform',function(require, exports, module) {
 				return;
 			}
 			if (type == 1) {
-				$.box.hide(Validform.util.boxHandle);
-				Validform.util.boxHandle = $.box.msg(msg, {
+				box.hide(Validform.util.boxHandle);
+				Validform.util.boxHandle = box.msg(msg, {
 					delay: 2e3,
 					color: color
 				});
